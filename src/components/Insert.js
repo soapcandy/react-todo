65d01c0cbd01088270b0512aad689632b58d42bf
박지훈 <octi97@naver.com>
2024-06-04 20:36:26 +0900

    inputRef로 input 포커스

--- src/components/Insert.js
+++ src/components/Insert.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Insert({ addInput }) {
   const [value, setValue] = useState("");
   const [id, setId] = useState(0);
+  const inputRef = useRef(null)
+
   const handleAdd = () => {
     setId(id + 1);
     const newAdd = { id, value };
     addInput(newAdd);
     setValue("")
+    inputRef.current.focus()
   };
 
   const handleKeyPress = (e) => {
@@ -20,6 +23,7 @@ function Insert({ addInput }) {
     <div>
       <input
         type="text"
+        ref={inputRef}
         value={value}
         onChange={(e) => setValue(e.target.value)}
         onKeyDown={handleKeyPress}
